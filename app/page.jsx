9e82c58ed5f727e6135b7a1853bd74b57ee6a2be
Jimena Hernández <her21199@uvg.edu.gt>
2024-05-14 09:50:39 -0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 export default function Home() {
   return (
-    <main className="bg-redish flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="w-full flex flex-col items-center justify-center gap-4">
-        <img src="/images/logo-dcc.svg" alt="Design Culture Company" className="w-72" />
-        <div className="flex flex-col items-center justify-center gap-2">
-          <span className="text-white text-2xl roboto-thin">we&apos;ll be right back</span>
-          <a href="mailto:contact@designculture.company" target="_blank" rel="noreferrer" className="text-white text-xl roboto-thin">contact@designculture.company</a>
+    <main className="bg-redish flex min-h-screen flex-col items-center justify-center md:p-24 p-12">
+      <div className="w-full flex flex-col items-center justify-center gap-6">
+        <img src="/images/logo-dcc.svg" alt="Design Culture Company" className="w-[600px] md:w-[350px]" />
+        <div className="flex flex-col items-center justify-center">
+          <span className="text-white text-xl md:text-2xl roboto-thin">we&apos;ll be right back</span>
+          <a href="mailto:contact@designculture.company" target="_blank" rel="noreferrer" className="text-white text-md md:text-xl roboto-thin">contact@designculture.company</a>
         </div>
       </div>
     </main>
