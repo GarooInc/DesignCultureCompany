450f162365a923b7578845ae7bddb387f2780546
Jimena Hernández <her21199@uvg.edu.gt>
2024-05-14 10:04:00 -0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="bg-redish flex min-h-screen flex-col items-center justify-center md:p-24 p-12">
       <div className="w-full flex flex-col items-center justify-center gap-6">
-        <img src="/images/logo-dcc.svg" alt="Design Culture Company" className="w-[300px] md:w-[350px]" />
+        <img src="/images/logo-dcc.svg" alt="Design Culture Company" className="w-[275px] md:w-[350px]" />
         <div className="flex flex-col items-center justify-center">
-          <span className="text-white text-xl md:text-2xl roboto-thin">we&apos;ll be right back</span>
-          <a href="mailto:contact@designculture.company" target="_blank" rel="noreferrer" className="text-white text-md md:text-xl roboto-thin">contact@designculture.company</a>
+          <span className="text-white text-xl md:text-2xl roboto-light">we&apos;ll be right back</span>
+          <a href="mailto:contact@designculture.company" target="_blank" rel="noreferrer" className="text-white text-md md:text-xl roboto-light">contact@designculture.company</a>
         </div>
       </div>
     </main>
